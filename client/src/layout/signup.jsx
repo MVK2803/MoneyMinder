d7c96de5af6c6b74b645dd7599c264c8b569c108
Tom Antony <antonytom3477@gmail.com>
2023-12-03 19:59:27 +0530

    commit

--- client/src/layout/signup.jsx
+++ client/src/layout/signup.jsx
@@ -1,9 +1,9 @@
 // SignUp.jsx
 import React, { useState } from 'react';
 
-import user_icon from '../assets/person.png'
-import email_icon from '../assets/email.png'
-import password_icon from '../assets/password.png'
+import user_icon from '../assets/person-bounding-box.svg'
+import email_icon from '../assets/envelope-at.svg'
+import password_icon from '../assets/lock.svg'
 
 function SignUp() {
     const [formData, setFormData] = useState({
@@ -27,18 +27,18 @@ function SignUp() {
             <div className='header'>
                 <div className='text-center p-10 text-3xl'>MoneyMinder</div>
             </div>
-            <div className='items-center flex flex-col justify-center'>
+            <div className='items-center flex flex-col justify-center mb-8'>
                 <div className=' p-5 flex items-center space-x-5  w-fit'>
-                    <img src={user_icon} alt="" classname='' />
-                    <input type="text" name="" id="" className=' border-2 p-1' placeholder='Enter Name' />
+                    <img src={user_icon} alt="" classname='w-30 h-30' />
+                    <input type="text" name="" id="" className=' border-4 border-gray-200 rounded-md p-1' placeholder='Enter Name' />
                 </div>
                 <div className=' p-5 flex items-center space-x-5  w-fit'>
                     <img src={email_icon} alt="" />
-                    <input type="email" name="" id="" className='border p-1' placeholder='Enter E-mail' />
+                    <input type="email" name="" id="" className='border-4 border-gray-200 rounded-md p-1' placeholder='Enter E-mail' />
                 </div>
                 <div className=' p-5 flex items-center space-x-5  w-fit'>
                     <img src={password_icon} alt="" />
-                    <input type="password" name="" id="" className='border p-1' placeholder='Enter Password' />
+                    <input type="password" name="" id="" className='border-4 border-gray-200 rounded-md p-1' placeholder='Enter Password' />
                 </div>
             </div>
             <div className='flex justify-center items-center'>
