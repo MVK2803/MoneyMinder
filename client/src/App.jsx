3ccce4e590352f3e6affd066004b567e2f04c9d6
Jefin Reji <jefinreji432@gmail.com>
2023-12-03 22:50:20 +0530

    Added SignUp

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
-import LandingPage from './layout/LandingPage'
-
+import LandingPage from './pages/LandingPage'
+import LoginPage from './pages/LoginPage'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <div className="absolute w-full h-full bg-slate-300  flex items-center justify-center">
-      <LandingPage/>
+      <LoginPage/>
       </div>
 
     </>
